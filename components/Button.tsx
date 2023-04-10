e9fe366928ccfd2ceb7514bb19bc64340669f32e
NEDONION <nedjiachenghu@gmail.com>
2023-04-10 03:01:44 -0500

    feat: login and register modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -30,15 +30,15 @@ const Button: React.FC<ButtonProps> = ({
         transition
         border-2
         ${fullWidth ? 'w-full' : 'w-fit'}
-        ${secondary ? 'bg-white' : 'bg-sky-500'}
-        ${secondary ? 'text-black' : 'text-white'}
-        ${secondary ? 'border-black' : 'border-sky-500'}
+        ${secondary ? 'bg-black' : 'bg-sky-500'}
+        ${secondary ? 'text-white' : 'text-black'}
+        ${secondary ? 'border-white' : 'border-sky-500'}
         ${large ? 'text-xl' : 'text-md'}
         ${large ? 'px-5' : 'px-4'}
         ${large ? 'py-3' : 'py-2'}
         ${outline ? 'bg-transparent' : ''}
-        ${outline ? 'border-white' : ''}
-        ${outline ? 'text-white' : ''}
+        ${outline ? 'border-black' : ''}
+        ${outline ? 'text-black' : ''}
       `}
     >
       {label}
