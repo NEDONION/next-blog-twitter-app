c5556eb1ff3d7b750cfb44689c53e9afa1ee8edc
NEDONION <nedjiachenghu@gmail.com>
2023-04-26 22:24:54 -0700

    feat: prisma update and users page

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Prisma Setup
+
+```bash
+npx prisma init
+
+# make sure to have your current IP address in whitelist
+npx prisma db push 
+```
--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -18,7 +18,7 @@ const FollowBar = () => {
                 <div key={user.id} className="flex flex-row gap-4">
                     <Avatar userId={user.id} />
                     <div className="flex flex-col">
-                        <p className="text-white font-semibold text-sm">{user.name}</p>
+                        <p className="text-black font-semibold text-sm">{user.name}</p>
                         <p className="text-neutral-400 text-sm">@{user.username}</p>
                     </div>
                 </div>
