7263cf31dbc60d777be5481605887cc3ef16a64d
NEDONION <nedjiachenghu@gmail.com>
2023-04-27 14:11:48 -0700

    feat: change color css for follow bar

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -11,7 +11,7 @@ const FollowBar = () => {
 
   return (
     <div className="px-6 py-4 hidden lg:block">
-      <div className="bg-gray-300 rounded-xl p-4 opacity-80">
+      <div className="bg-gray-100 rounded-xl p-4 opacity-80">
         <h2 className="text-black text-xl font-bold">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
             {users.map((user: Record<string, any>) => (
